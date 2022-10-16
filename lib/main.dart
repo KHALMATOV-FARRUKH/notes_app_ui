937da9a6be1a3be457f31fb613ecfdbe31c67bc3
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-10-16 23:33:52 +0500

    The following packages have been added: flutter pub add google_fonts flutter pub add lottie flutter pub add font_awesome_flutter flutter pub add flutter_staggered_animations flutter pub and flutter_staggered_grid_view
    Created by:
    all_view.dart
    curved_box.dart
    folder_view.dart
    home_view.dart
    note_widget.dart
    files
    
    Added folder:
    assets
    in which:
    Added files:
    folder.json
    notes.json
    travel.jpg

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:notes_app_ui/app_colors.dart';
 import 'package:notes_app_ui/views/landing_view.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +13,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              actionsIconTheme: IconThemeData(color: AppColors.lightTextColor),
+              centerTitle: false,
+              backgroundColor: Colors.transparent,
+              shadowColor: Colors.transparent),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: AppColors.buttonColor),
+          textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(primary: AppColors.lightTextColor)),
+          tabBarTheme: const TabBarTheme(
+              indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(width: 3, color: AppColors.orange)),
+              indicatorSize: TabBarIndicatorSize.label,
+              labelColor: AppColors.orange),
+          fontFamily: GoogleFonts.poppins().fontFamily,
+          backgroundColor: AppColors.containerBackground,
+          scaffoldBackgroundColor: AppColors.scaffoldBackground,
+          brightness: Brightness.dark,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(100, 50),
+              primary: AppColors.buttonColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          )),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/views/landing_view.dart
+++ lib/views/landing_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app_ui/app_colors.dart';
+import 'package:notes_app_ui/views/home_view.dart';
+import 'package:notes_app_ui/views/note_widget.dart';
 
 class LandingView extends StatelessWidget {
   const LandingView({Key? key}) : super(key: key);
@@ -12,16 +14,48 @@ class LandingView extends StatelessWidget {
         children: [
           SizedBox(
             height: h * 0.5,
+            child: const NoteWidget(),
           ),
           const SizedBox(
             height: 32,
           ),
           Text(
-            'Ежедневные заметки',
+            'Ежедневные Заметки',
             style: Theme.of(context)
                 .textTheme
                 .headlineMedium!
                 .copyWith(color: AppColors.white),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 56),
+            child: Text(
+              'Делайте заметки, напоминания, устанавливайте цели, собирайте ресурсы',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(color: AppColors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: h * 0.1,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const HomeView(),
+              ));
+            },
+            child: Text(
+              'Начать',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          SizedBox(
+            height: h * 0.1,
           ),
         ],
       ),
